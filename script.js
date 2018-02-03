b205ceb566b95035d56a894d4d1edff95f21183b
ATarsa <smb909@gmail.com>
2018-02-03 08:46:37 +0000

    add buttons which are changing hovering color

--- script.js
+++ script.js
@@ -23,11 +23,10 @@ https://stackoverflow.com/questions/20308270/create-multiple-divs-with-the-same-
 			board.appendChild(myDivs[i]);
 		}
 	};
-/*
-Using Css variables in js
-with a help of this article:
- https://medium.com/@fionnachan/dynamic-number-of-rows-and-columns-with-css-grid-layout-and-css-variables-cb8e8381b6f2
-*/ 
+		/*
+		Using Css variables in js with a help of this article:
+		https://medium.com/@fionnachan/dynamic-number-of-rows-and-columns-with-css-grid-layout-and-css-variables-cb8e8381b6f2
+		*/ 
 
 	let htmlStyles = window.getComputedStyle(document.querySelector("html"));
 	let columnNumber = parseInt(htmlStyles.getPropertyValue("--columnNumber"));
@@ -36,10 +35,41 @@ with a help of this article:
 	
 	var board = document.getElementById("board");
 	board.addEventListener("mouseover",function(event){
-			event.target.style.backgroundColor = "black";
+			if (board.classList[1] === "randomColor"){
+				event.target.style.backgroundColor = createRandomColor();
+			} else if (board.classList[1] === "eraser"){
+				event.target.style.backgroundColor = "white"
+
+			} else 	{
+				event.target.style.backgroundColor = "black";
+			}
+			
 							
-		});
+	});
+
+	/*
+	Helpers functions
+	*/
+	// A function to create random number from min to max
+	function getRandomInt(min,max){
+		min = Math.ceil(min);
+		max = Math.floor(max)
+		return Math.floor(Math.random()* (max - min) + min);
 	
+	};
+
+	function createRandomColor(){
+		var r = getRandomInt(0,256);
+		var g = getRandomInt(0,256);
+		var b = getRandomInt(0,256);
+		return "rgb(" + r + "," + g + "," + b + ")";
+	}
+
+	function removeClass(){
+		var classToBeRemoved = board.classList.item(1);
+		board.classList.remove(classToBeRemoved);
+	}
+
 	var handlers = {
 		resetGrid: function(){
 
@@ -48,8 +78,33 @@ with a help of this article:
 				gridNumber = parseInt(prompt('how big the grid?'),10);
 				document.documentElement.style.setProperty("--columnNumber", gridNumber);
 				createBoardContainer(gridNumber);
+				removeClass();
+			});
+		},
+		blackColor: function(){
+			var blackColorButtun = document.getElementById("blackColor");
+			blackColorButtun.addEventListener("click", function(){
+				removeClass();
+				board.classList.add("blackColor");
+			});
+		},
+		randomColor: function(){
+			
+			var randomColorButton = document.getElementById("randomColor");
+			randomColorButton.addEventListener("click",function(){
+				removeClass();
+				board.classList.add("randomColor");
+								
+			});
+		},
+		eraser: function(){
+			var eraserButton = document.getElementById("eraser");
+			eraserButton.addEventListener("click", function(){
+				removeClass();
+				board.classList.add("eraser");
 			});
 		}
+		
 	}
 
 
